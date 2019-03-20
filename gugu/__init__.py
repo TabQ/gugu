2adb1c58424562faebb121de6cf4e9813ebc6f65
root <16621596@qq.com>
2019-03-20 15:36:30 +0800

    first commit

---
+++ gugu/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+__version__ = '0.1.5'
+__author__ = 'TabQ'
+
+
+from gugu.base import Base
+
+from gugu.billboard import BillBoard
+
+from gugu.classify import Classify
+
+from gugu.lowriskintarb import LowRiskIntArb
+
+from gugu.macro import Macro
+
+from gugu.marketdata import MarketData
+
+from gugu.reference import Reference
+
+from gugu.stockdata import StockData
+
+from gugu.stockinfo import StockInfo
+
+from gugu.utility import Utility
+
